c7b3bcc96fd48aa5e4111e7197d1fee21c366d25
David Makuni Ocelot Ubuntu <daviemakz@gmail.com>
2017-09-25 23:50:19 +0100

    barebones

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-"use strict";
+'use strict';
+
+// Load Modules
+import poolManager from 'mysql-connection-pool-manager';
+import {} from './lib';
 
 // Build instance
-class Instance {
-
+class DBSync {
   // Initial constructor
-  constructor(options) {
+  constructor(db, options = {}) {
     // Declare variables
-    this.currentDbState = {};
-    this.configDbState = {};
+    this.currentDBState = {};
+    this.configDBState = {};
+    // Bind methods
+    this.sync = this.sync.bind(this);
+    // Default settings
+    this.config = Object.assign(
+      {
+        idleCheckInterval: 1000,
+        maxConnextionTimeout: 30000,
+        idlePoolTimeout: 3000,
+        errorLimit: 5,
+        preInitDelay: 50,
+        sessionTimeout: 60000,
+        mySQLSettings: db,
+        databaseKey: 'Database',
+        fieldKey: 'Name',
+        userKey: 'User',
+        fieldKeyType: 'Type',
+        fieldKeyDefault: 'Default',
+        fieldKeyKey: 'Key',
+        fieldKeyExtra: 'Extra',
+        filePath: undefined
+      },
+      options
+    );
+    // Assign mysql settings
+    this.mysqli = poolManager(this.config);
   }
 
+  // Syncronise the database and the configuration
+  sync() {
+    if (!this.config.filePath) {
+      throw new Error(`The file path is not defined!`);
+    }
+  }
 }
 
 // Exports
-module.exports = (options) => new Instance(options);
+module.exports = options => new DBSync(options);
